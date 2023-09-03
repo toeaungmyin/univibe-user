00e9afa860e96e45a062cb8bd16b8ddbfba310bd
toeaungmyin-noobie <toeaungmyin.official@gmail.com>
2023-09-04 01:09:44 +0630

    active pro

--- src/broadcast.js
+++ src/broadcast.js
@@ -4,8 +4,8 @@ import Pusher from 'pusher-js';
 const options = {
 	broadcaster: 'pusher',
 	authEndpoint:
-		// 'https://univibe-server-production-2df7.up.railway.app/broadcasting/auth',
-		'http://127.0.0.1:8000/broadcasting/auth',
+		'https://univibe-server-production-2df7.up.railway.app/broadcasting/auth',
+	// 'http://127.0.0.1:8000/broadcasting/auth',
 	key: 'ea35363792d83a5c687f', // Replace with your Pusher key
 	cluster: 'ap1', // Replace with your Pusher cluster
 	encrypted: true,
